ace6810011292fc32d37a9be0602575b9dab6926
deannafernandes34 <95317578+deannafernandes34@users.noreply.github.com>
2023-01-25 17:29:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,10 +10,6 @@ ReactDOM.render(
   <BrowserRouter>
   <>
   <Nav />
-    <Route exact path="/Apple"><News newsName="iphone" /></Route>
-    <Route path="/Tesla"><News newsName="tesla" /></Route>
-    <Route path="/Bitcoin"><News newsName="bitcoin" /></Route>
-    <Route path="/nasa"><News newsName="nasa" /></Route>
     <Route path="/upsc"><News newsName="upsc" /></Route>
   </>
   </BrowserRouter>,
